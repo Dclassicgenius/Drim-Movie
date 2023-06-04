7e4519eb1f4acbfd28d561ac98c865f4df6c8e32
Dclassicgenius <patamaechi1@gmail.com>
2023-06-04 16:33:26 +0300

    updated reviewAll props

--- src/components/Review/ReviewAll.tsx
+++ src/components/Review/ReviewAll.tsx
@@ -1,4 +1,3 @@
-import { useParams } from "react-router-dom";
 import { Review } from "./Review";
 import { PageHeader } from "../Layout/PageHeader";
 
@@ -9,14 +8,12 @@ export type ReviewAllProps = {
 };
 
 export function ReviewAll({ id, useDetail, detailType }: ReviewAllProps) {
-  const API_IMG = "https://image.tmdb.org/t/p/w500";
   return (
     <>
       <div className="">
         <PageHeader useDetail={useDetail} detailType={detailType} />
         <Review
           id={id}
-          API_IMG={API_IMG}
           displayCount={-1}
           useDetail={useDetail}
           detailType={detailType}
